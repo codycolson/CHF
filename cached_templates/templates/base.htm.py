b23d9a767235aaccdad2dde69344726bf9426755
Blake Wight <blake.wight@gmail.com>
2015-01-20 17:31:18 -0700

    Initial commit for our CHF Project.

---
+++ cached_templates/templates/base.htm.py
@@ -0,0 +1,67 @@
+# -*- coding:ascii -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1421798667.290233
+_enable_loop = True
+_template_filename = 'C:\\Users\\Blake\\test_dmp\\homepage\\templates/base.htm'
+_template_uri = 'base.htm'
+_source_encoding = 'ascii'
+import os, os.path, re
+_exports = ['content']
+
+
+from django_mako_plus.controller import static_files 
+
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        self = context.get('self', UNDEFINED)
+        request = context.get('request', UNDEFINED)
+        def content():
+            return render_content(context._locals(__M_locals))
+        __M_writer = context.writer()
+        __M_writer('\r\n')
+        __M_writer('\r\n')
+        static_renderer = static_files.StaticRenderer(self) 
+        
+        __M_locals_builtin_stored = __M_locals_builtin()
+        __M_locals.update(__M_dict_builtin([(__M_key, __M_locals_builtin_stored[__M_key]) for __M_key in ['static_renderer'] if __M_key in __M_locals_builtin_stored]))
+        __M_writer('\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n  <meta charset="UTF-8">\r\n  <head>\r\n    \r\n    <title>HomePage</title>\r\n    \r\n')
+        __M_writer('    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>\r\n    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"></link>\r\n    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></link>\r\n        \r\n    \r\n\r\n')
+        __M_writer('    ')
+        __M_writer(str( static_renderer.get_template_css(request, context)  ))
+        __M_writer('\r\n  \r\n  </head>\r\n  <body>\r\n  \r\n    <header>\r\n        <nav class="navbar navbar-default">\r\n            <div class="container-fluid">\r\n                <div class="navbar-header">\r\n                    <a class="navbar-brand" href="/"><span class="badge">new!</span>Colonial Heritage Foundation</a>\r\n                </div>\r\n                <div class="collapse navbar-collapse">\r\n                  <ul class="nav navbar-nav">\r\n                    <li><a href="/items">Browse</a></li>\r\n                    <li><a href="#/items">Rent </a></li>\r\n                  </ul>\r\n                  <ul class="nav navbar-nav navbar-right">\r\n                    <li data-toggle="modal" data-target="#loginModal"><a href="#">Login</a></li>\r\n                    <li><a href="#">Register</a></li>\r\n                  </ul>\r\n                </div>\r\n            </div>\r\n        </nav>\r\n    </header>\r\n      \r\n    <div class="container-fluid">\r\n        <div class="row">\r\n            <div class="col-md-2 left-nav">\r\n                <ul>\r\n                    <li><a href="/about">about</a></li>\r\n                    <li><a href="/contact">contact</a></li>\r\n                    <li><a href="/terms">terms</a></li>\r\n                </ul>\r\n            </div>\r\n            <div class="col-md-10">\r\n                ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
+            context['self'].content(**pageargs)
+        
+
+        __M_writer('\r\n            </div>\r\n        </div>\r\n    </div>\r\n      <div class="modal hide" id="loginModal">\r\n      <div class="modal-dialog">\r\n        <div class="modal-content">\r\n          <div class="modal-header">\r\n              header\r\n          </div>\r\n          <div class="modal-body">\r\n              body\r\n          </div>\r\n          <div class="modal-footer">\r\n            footer\r\n          </div>\r\n        </div>\r\n      </div>\r\n    </div>\r\n      \r\n    <footer>\r\n        <div class="well">Blake Wight</div>\r\n    </footer>\r\n  \r\n')
+        __M_writer('    ')
+        __M_writer(str( static_renderer.get_template_js(request, context)  ))
+        __M_writer('  \r\n  </body>\r\n</html>')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content():
+            return render_content(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n                ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "C:\\Users\\Blake\\test_dmp\\homepage\\templates/base.htm", "line_map": {"33": 5, "34": 15, "35": 22, "36": 22, "37": 22, "42": 58, "43": 83, "44": 83, "45": 83, "16": 4, "18": 0, "51": 57, "57": 57, "27": 2, "28": 4, "29": 5, "63": 57}, "source_encoding": "ascii", "uri": "base.htm"}
+__M_END_METADATA
+"""
---
+++ cached_templates/templates/index.html.py
@@ -0,0 +1,68 @@
+# -*- coding:ascii -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1421779205.517164
+_enable_loop = True
+_template_filename = 'C:\\Users\\Blake\\test_dmp\\homepage\\templates/index.html'
+_template_uri = 'index.html'
+_source_encoding = 'ascii'
+import os, os.path, re
+_exports = ['content']
+
+
+def _mako_get_namespace(context, name):
+    try:
+        return context.namespaces[(__name__, name)]
+    except KeyError:
+        _mako_generate_namespaces(context)
+        return context.namespaces[(__name__, name)]
+def _mako_generate_namespaces(context):
+    pass
+def _mako_inherit(template, context):
+    _mako_generate_namespaces(context)
+    return runtime._inherit_from(context, 'base.htm', _template_uri)
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def content():
+            return render_content(context._locals(__M_locals))
+        __M_writer = context.writer()
+        __M_writer('\r\n\r\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
+            context['self'].content(**pageargs)
+        
+
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def content():
+            return render_content(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n    <div id="carousel-index" class="carousel slide" data-ride="carousel">\r\n      <!-- Indicators -->\r\n      <ol class="carousel-indicators">\r\n        <li data-target="#carousel-index" data-slide-to="0" class="active"></li>\r\n        <li data-target="#carousel-index" data-slide-to="1"></li>\r\n        <li data-target="#carousel-index" data-slide-to="2"></li>\r\n      </ol>\r\n\r\n      <!-- Wrapper for slides -->\r\n      <div class="carousel-inner" role="listbox">\r\n        <div class="item active">\r\n          <img src="')
+        __M_writer(str(STATIC_URL))
+        __M_writer('hompage/media/mayflower.jpg" alt="The Mayflower">\r\n          <div class="carousel-caption">\r\n            <p>The Colonial Heritage Foundation is dedicated to the preservation of the values, culture, skills and history of America\'s founding.</p>\r\n          </div>\r\n        </div>\r\n        <div class="item">\r\n          <img src="')
+        __M_writer(str(STATIC_URL))
+        __M_writer('hompage/media/colonials.jpg" alt="Colonials">\r\n          <div class="carousel-caption">\r\n            <p>Items are available for rent as well as for sale.</p>\r\n          </div>\r\n        </div>\r\n        <div class="item">\r\n          <img src="')
+        __M_writer(str(STATIC_URL))
+        __M_writer('hompage/media/artisan.jpg" alt="...">\r\n          <div class="carousel-caption">\r\n            <p>Custom orders can be made and filled by skilled artisans.</p>\r\n          </div>\r\n        </div>\r\n      </div>\r\n\r\n      <!-- Controls -->\r\n      <a class="left carousel-control" href="#carousel-index" role="button" data-slide="prev">\r\n        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>\r\n        <span class="sr-only">Previous</span>\r\n      </a>\r\n      <a class="right carousel-control" href="#carousel-index" role="button" data-slide="next">\r\n        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>\r\n        <span class="sr-only">Next</span>\r\n      </a>\r\n    </div>\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"uri": "index.html", "filename": "C:\\Users\\Blake\\test_dmp\\homepage\\templates/index.html", "source_encoding": "ascii", "line_map": {"64": 58, "35": 1, "52": 3, "53": 15, "54": 15, "55": 21, "56": 21, "57": 27, "58": 27, "27": 0, "45": 3}}
+__M_END_METADATA
+"""
